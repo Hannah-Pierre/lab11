464b6cbca132283efc4389df9612f6ed8d3c7fef
Hannah-Pierre <s581460@student.roundrockisd.org>
2014-10-30 15:01:18 -0500

    100pt not finished

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -22,8 +22,8 @@ def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        drawpad.move(circle, 10, 10)
+    if x1 > drawpad.winfo_width(): 
+        drawpad.move(circle, 800, 0)
     elif x1 < 0:
         direction = 1
     #Move our oval object by the value of direction
